401654126b1256cca8a862800826109d337948ae
KernKI <kernki@local>
2026-07-18 02:45:59 +0200

    fix: add PrismaPlugin webpack workaround (bundles engine binary for Netlify)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...(config.plugins || []), new PrismaPlugin()];
+    }
+    return config;
+  },
 };
 
 // PWA-Konfiguration.
